800fe33c9ad3d5e8ce56aa9b8f0b45edc674ba98
snamiki1212 <snamiki1212@gmail.com>
2021-10-20 04:46:08 -0700

    refactor: rename path

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,5 +1,5 @@
 pub mod articles;
 pub mod auth;
 pub mod profiles;
-pub mod tags;
+pub mod tag;
 pub mod users;
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,51 @@
 use actix_web::{web, App, HttpServer};
-mod api;
+mod app;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(
-            web::scope("/api")
-                .service(web::scope("/tags").service(api::tags::index))
-                .service(
-                    web::scope("/users")
-                        .service(api::auth::signin)
-                        .service(api::auth::signup),
-                )
-                .service(
-                    web::scope("/user")
-                        .service(api::users::me)
-                        .service(api::users::update),
-                )
-                .service(
-                    web::scope("/profiles")
-                        .service(api::profiles::show)
-                        .service(api::profiles::follow)
-                        .service(api::profiles::unfollow),
-                )
-                .service(
-                    web::scope("/articles/{id}/comments")
-                        .service(api::articles::comments::index)
-                        .service(api::articles::comments::create)
-                        .service(api::articles::comments::delete),
-                )
-                .service(
-                    web::scope("/articles/{id}/favorites")
-                        .service(api::articles::favorites::favorite)
-                        .service(api::articles::favorites::unfavorite),
-                )
-                .service(
-                    web::scope("/articles")
-                        .service(api::articles::index)
-                        .service(api::articles::feed)
-                        .service(api::articles::show)
-                        .service(api::articles::create)
-                        .service(api::articles::update)
-                        .service(api::articles::delete),
-                ),
-        )
+        App::new()
+            // .data()
+            .service(
+                web::scope("/api")
+                    .service(web::scope("/tags").service(app::tag::api::index))
+                    .service(
+                        web::scope("/users")
+                            .service(app::auth::signin)
+                            .service(app::auth::signup),
+                    )
+                    .service(
+                        web::scope("/user")
+                            .service(app::users::me)
+                            .service(app::users::update),
+                    )
+                    .service(
+                        web::scope("/profiles")
+                            .service(app::profiles::show)
+                            .service(app::profiles::follow)
+                            .service(app::profiles::unfollow),
+                    )
+                    .service(
+                        web::scope("/articles/{id}/comments")
+                            .service(app::articles::comments::index)
+                            .service(app::articles::comments::create)
+                            .service(app::articles::comments::delete),
+                    )
+                    .service(
+                        web::scope("/articles/{id}/favorites")
+                            .service(app::articles::favorites::favorite)
+                            .service(app::articles::favorites::unfavorite),
+                    )
+                    .service(
+                        web::scope("/articles")
+                            .service(app::articles::index)
+                            .service(app::articles::feed)
+                            .service(app::articles::show)
+                            .service(app::articles::create)
+                            .service(app::articles::update)
+                            .service(app::articles::delete),
+                    ),
+            )
     })
     .bind("127.0.0.1:8080")?
     .run()
