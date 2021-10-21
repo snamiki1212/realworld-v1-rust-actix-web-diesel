d5022f163b672ec5ee30df3c5c8bf0ddbb905f95
snamiki1212 <snamiki1212@gmail.com>
2021-10-20 23:53:19 -0700

    feat: add user migration

--- src/schema.rs
+++ src/schema.rs
@@ -6,3 +6,19 @@ table! {
         updated_at -> Timestamp,
     }
 }
+
+table! {
+    users (id) {
+        id -> Uuid,
+        email -> Text,
+        bio -> Text,
+        image -> Nullable<Text>,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+    }
+}
+
+allow_tables_to_appear_in_same_query!(
+    tags,
+    users,
+);
