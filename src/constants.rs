8266e8eeb9bc6faac1624c7bae3fc7550f58b2d7
snamiki1212 <snamiki1212@gmail.com>
2021-10-23 12:52:42 -0700

    fix: ignore auth path

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,4 @@
-pub const IGNORE_AUTH_ROUTES: [&str; 3] = ["/api/tags", "/api/auth/signup", "/api/auth/login"];
+pub const IGNORE_AUTH_ROUTES: [&str; 3] = ["/api/tags", "/api/users", "/api/users/login"];
 // pub const MESSAGE_INVALID_TOKEN: &str = "Invalid token, please login again";
 // pub const EMPTY: &str = "";
 pub const AUTHORIZATION: &str = "Authorization";
