1abb52784b4b284d6fd302f23f836327c66f114d
snamiki1212 <snamiki1212@gmail.com>
2021-10-25 14:41:52 -0700

    refactor: create-article service logic

--- src/app/article/service.rs
+++ src/app/article/service.rs
@@ -1,6 +1,5 @@
 use crate::app::article::model::{Article, NewArticle};
 use crate::app::article::tag::model::{NewTag, Tag};
-use crate::app::user::model::User;
 use diesel::pg::PgConnection;
 
 pub fn create(
@@ -9,21 +8,26 @@ pub fn create(
     tag_list: &Option<Vec<String>>,
 ) -> (Article, Vec<Tag>) {
     let article = Article::create(&conn, &new_article);
+    let tag_list = create_tag_list(&conn, &tag_list, &article);
+    (article, tag_list)
+}
 
-    let tag_list = match tag_list {
-        Some(tag_list) => {
-            let tag_list = tag_list
+fn create_tag_list(
+    conn: &PgConnection,
+    tag_list: &Option<Vec<String>>,
+    article: &Article,
+) -> Vec<Tag> {
+    tag_list
+        .as_ref()
+        .map(|tag_list| {
+            let records = tag_list
                 .iter()
                 .map(|tag| NewTag {
                     name: &tag,
                     article_id: &article.id,
                 })
                 .collect();
-            let tag_list = Tag::create(&conn, tag_list);
-            tag_list
-        }
-        None => vec![],
-    };
-
-    (article, tag_list)
+            Tag::create(&conn, records)
+        })
+        .unwrap_or(vec![])
 }
