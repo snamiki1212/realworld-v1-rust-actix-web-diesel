8e3f324ef9fc2eb72fb86088317b1cbe5b3b5765
snamiki1212 <snamiki1212@gmail.com>
2021-10-29 21:18:01 -0700

    feat: add test

--- src/utils/converter.rs
+++ src/utils/converter.rs
@@ -3,3 +3,13 @@ use convert_case::{Case, Casing};
 pub fn to_kebab(text: &str) -> String {
     text.to_case(Case::Kebab)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn str_to_kebab() {
+        assert_eq!("this-is-blog-title", to_kebab("this is blog title"));
+    }
+}
