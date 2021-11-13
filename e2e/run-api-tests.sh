28b96232fb945887d58b5b316da83e3178ca7e01
snamiki1212 <snamiki1212@gmail.com>
2021-11-12 17:34:02 -0800

    fix: e2e path

--- e2e/run-api-tests.sh
+++ e2e/run-api-tests.sh
@@ -8,7 +8,7 @@ USERNAME=${USERNAME:-u`date +%s`}
 EMAIL=${EMAIL:-$USERNAME@mail.com}
 PASSWORD=${PASSWORD:-password}
 
-npx newman run $SCRIPTDIR/Conduit.postman_collection.json \
+npx newman run $SCRIPTDIR/e2e/Conduit.postman_collection.json \
   --delay-request 500 \
   --global-var "APIURL=$APIURL" \
   --global-var "USERNAME=$USERNAME" \
