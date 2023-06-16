2a065215cb1f8c7fd5e5261771b0215552a17392
snamiki1212 <snamiki1212@gmail.com>
2023-06-16 12:24:48 +0900

    refactor: remove pool in app-state

--- src/app/drivers/middlewares/state.rs
+++ src/app/drivers/middlewares/state.rs
@@ -1,26 +1,14 @@
-use crate::error::AppError;
 use crate::utils::db::DbPool;
 use crate::utils::di::DiContainer;
-use diesel::pg::PgConnection;
-use diesel::r2d2::{ConnectionManager, PooledConnection};
-
-type AppConn = PooledConnection<ConnectionManager<PgConnection>>;
 
 #[derive(Clone)]
 pub struct AppState {
-    #[deprecated]
-    pub pool: DbPool,
     pub di_container: DiContainer,
 }
 
 impl AppState {
     pub fn new(pool: DbPool) -> Self {
         let di_container = DiContainer::new(&pool);
-        Self { pool, di_container }
-    }
-
-    pub fn get_conn(&self) -> Result<AppConn, AppError> {
-        let conn = self.pool.get()?;
-        Ok(conn)
+        Self { di_container }
     }
 }
