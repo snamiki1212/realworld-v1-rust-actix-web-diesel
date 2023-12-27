15bc63ebbecbb3e2ecf01a4e492627f251e9541b
snamiki1212 <snamiki1212@gmail.com>
2023-12-27 23:00:21 +0900

    fix: update e2e

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,9 @@ WORKDIR /app
 
 COPY . .
 
+# Define Healthcheck fot the container
+HEALTHCHECK --interval=5s --timeout=5s --start-period=5s --retries=20 CMD [ "curl","http://0.0.0.0:8080/api/healthcheck" ]
+
 # Upgrade the system and install dependencies for PostgreSQL
 RUN apt-get update && \
   apt-get upgrade -y -o DPkg::Options::=--force-confold && \
--- e2e/run-api-tests.sh
+++ e2e/run-api-tests.sh
@@ -9,11 +9,11 @@ EMAIL=${EMAIL:-$USERNAME@mail.com}
 PASSWORD=${PASSWORD:-password}
 
 # Ready for E2E
-# npm install -g newman
+npm install -g newman
 
 # run E2E test
 echo "running e2e..."
-npx newman run ./Conduit.postman_collection.json \
+npx newman run ./e2e/Conduit.postman_collection.json \
   --delay-request 500 \
   --global-var "APIURL=$APIURL" \
   --global-var "USERNAME=$USERNAME" \
